f533202f96a8c9456f1d9859b6cca92110ad042f
rolpppp <rlgarces@up.edu.ph>
2026-05-11 09:52:55 +0800

    feat: CRUD operations implemented

--- app/src/main/java/dev/rolp/runnerz/run/RunController.java
+++ app/src/main/java/dev/rolp/runnerz/run/RunController.java
@@ -7,10 +7,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -31,18 +33,12 @@ public class RunController {
     
     // GET
     @GetMapping("/runs")
-    List<Run> findAll(){
+    private List<Run> findAll(){
         return this.runRepo.findAll();
     }
 
-    // POST
-    @PostMapping("/runs/add")
-    void addRun(Run run){
-        runRepo.add(run);
-    }
-
     @GetMapping("/runs/{id}")
-    Optional<Run> findById(@PathVariable int id){
+    private Optional<Run> findById(@PathVariable int id){
 
         Optional<Run> run = this.runRepo.findById(id);
 
@@ -52,4 +48,28 @@ public class RunController {
             return run;
         }
     }
+
+    // POST
+    @PostMapping("/runs")
+    private void addRun(@RequestBody Run run){
+        runRepo.add(run);
+    }
+
+    // DELETE
+    @DeleteMapping("/runs/{id}")
+    private void deleteRun(@PathVariable int id){
+        this.runRepo.deleteRun(id);
+    }
+
+    @PutMapping("/runs/{id}")
+    private void updateRun(@PathVariable int id, @RequestBody Run run){
+        Optional<Run> runTemp = this.runRepo.findById(id);
+
+        if (runTemp.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }else{
+            this.runRepo.updateRun(id, run);
+        }
+    }
+    
 }
